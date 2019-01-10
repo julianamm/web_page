9858b3d2f9c61b387b08da41f301b246ce5ee29b
Juliana Martins <julianamm88@gmail.com>
2019-01-09 22:27:12 -0800

    add drawings section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
-// Preloader
+/* =============
+    Preloader
+================ */
 $(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 })
+
+/* ==========================
+    Drawings - Isotope
+============================= */
+$(window).on('load', function () {
+    // Initialize Isotope
+    $("#isotope-container").isotope({});
+    // filter items on button click
+    $("#isotope-filters").on('click', 'button', function () {
+        // get filter value
+        var filterValue = $(this).attr('data-filter');
+        // filter drawings
+        $("#isotope-container").isotope({
+            filter: filterValue
+        });
+        // active button
+        $("#isotope-filters").find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+/* ==========================
+    Drawings - MagnificPopup
+============================= */
+$(function () {
+    $("#drawings-wrapper").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
