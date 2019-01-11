48842442bb0b364604f1b8ecdddb8496be139296
Juliana Martins <julianamm88@gmail.com>
2019-01-10 22:30:00 -0800

    add navigation and animations

--- js/script.js
+++ js/script.js
@@ -38,3 +38,81 @@ $(function () {
         }
     });
 });
+
+// Navigation
+// Show & Hide white navigation
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+        // show/hide nav on window's scroll
+        showHideNav();
+    })
+
+    function showHideNav() {
+        if ($(window).scrollTop() > 50) {
+            // Show white nav
+            $('nav').addClass('white-nav-top')
+
+            //show logo name initials
+            $('a.name-logo').text('JM').addClass('initials')
+
+            // show back to top btn
+            $('#back-to-top').fadeIn()
+        } else {
+            // Hide white nav
+            $('nav').removeClass('white-nav-top')
+
+            //show logo name
+            $('a.name-logo').text('JuJu de Morais').removeClass('initials')
+
+            // hide back to top btn
+            $('#back-to-top').fadeOut()
+        }
+    }
+});
+
+// Smooth Scrolling
+$(function () {
+    $('a.smooth-scroll').click(function (e) {
+        e.preventDefault();
+        // get section id like #about, #service, #work, #team, etc.
+        let section_id = $(this).attr('href')
+        $('html, body').animate({
+            scrollTop: $(section_id).offset().top - 64
+        }, 1250, "easeInOutExpo")
+    })
+})
+
+// Mobile Menu
+$(function () {
+
+    // Show mobile nav
+    $("#mobile-nav-open-btn").click(function () {
+        $("#mobile-nav").css("height", "100%");
+    });
+
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function () {
+        $("#mobile-nav").css("height", "0%");
+    });
+
+});
+
+// Animation Wow
+// animate on scroll 
+$(function () {
+    new WOW().init();
+})
+
+// home animation on page load
+$(window).on('load', function () {
+    $('a.name-logo').addClass('animated bounceInDown')
+    $('#home-heading-1').addClass('animated fadeInRightBig')
+    $('#home-heading-2 span').addClass('animated fadeInLeftBig')
+    $('#home-text').addClass('animated jackInTheBox')
+    $('#home-btn').addClass('animated zoomIn')
+    $('#arrow-down i').addClass('animated fadeInDown infinite')
+})
