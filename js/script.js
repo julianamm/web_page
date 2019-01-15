b8fe4250806727cb7ad0197de9701766452e5790
Juliana Martins <julianamm88@gmail.com>
2019-01-14 17:25:53 -0800

    add projects section

--- js/script.js
+++ js/script.js
@@ -7,6 +7,32 @@ $(window).on('load', function () {
 })
 
 /* ==========================
+    Projects
+============================= */
+$(function () {
+    $('#project-items').owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left fa-3x"></i>', '<i class="fa fa-angle-right fa-3x"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 3
+            }
+        }
+    });
+});
+
+/* ==========================
     Drawings - Isotope
 ============================= */
 $(window).on('load', function () {
